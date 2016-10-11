614c17ebb046f3cc9c2f491d346be78d2c1bb3b2
BAPLIE Viewer Admin <ediviewer@baplieviewer.com>
2016-10-11 10:00:51 -0700

    Modified generatePdfBaseUrlRoute

--- system/js/src/core/i18labels.js
+++ system/js/src/core/i18labels.js
@@ -3,7 +3,7 @@ export const LOADING_DATA = "Loading 3D Model...";
 export const INVALID_DATA_SOURCE = "Error: Missing data source.";
 export const ERROR_PARSING_JSON = "Error while parsing the JSON file.";
 
-export const CLICK_TO_CHANGE_COLORS = "Click on colors to change them";
+export const CLICK_TO_CHANGE_COLORS = "Click on colors to change them. A black dot means there is already a custom colour being used.";
 export const NO_COLOR_SETTINGS = "No color settings found. Will use random.";
 
 export const PRINTOPTS_TITLE = "Please select the document options";
